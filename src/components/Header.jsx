c72c8bfbf5b9202e2ae27e1af66b0032b1dcd26b
hick.hk <hick.hk@gmail.com>
2024-08-07 01:43:19 -0300

    nav quase feita

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+
 
 export default function header() {
   return (
-    <div className='justify-center text-center'>
-        <p>Operação</p>
-        <p>Materiais</p>
-        <p>Home</p>
-        <p>Bibliografia</p>
-        <p>Membros</p>
+    <div>
+
+      <section className='flex justify-center space-x-4 w-full h-16 bg-fundo_nav text-cinza_fonte rounded-b-2xl space-x-16' >
+        <div className='my-4 text-3xl'>D.A.R.P</div>
+        <div className='my-4 '>Operação</div>
+        <div className='my-4 '>Materiais</div>
+        <div className='my-4 '>Home</div>
+        <div className='my-4 '>Bibliografia</div>
+        <div className='my-4'>Membros</div>
+      </section>
+      
+        
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'cinza_fonte' : '#CACACA',
+        'fundo_nav' : '#1E1E1E'
+      },
+    },
   },
   plugins: [],
+
+
 }
