1a54e3e643a1b5d29006002b051eb1c9c52912a6
KayqueHenrry <102825082+KayqueHenrry@users.noreply.github.com>
2024-08-20 10:32:07 -0300

    Página Operação finalizada

--- src/pages/Operação.jsx
+++ src/pages/Operação.jsx
@@ -1,7 +1,79 @@
+import Prot1 from '../assets/prot1.png';
+import Prot2 from '../assets/prot2.png';
+import Prot3 from '../assets/prot3.png';
+import Prot4 from '../assets/prot4.png';
+import Prot5 from '../assets/prot5.png';
+
 export default function Operação() {
   return (
-    <div>
-      <h1>Operação</h1>
-    </div>
+    <main className='flex-col justify-center items-center'>
+
+      <section className='flex-col justify-center mt-[23px]'>
+          <h1 className='text-center text-[164px] max-lg:text-[70px]'>Operação</h1>
+          <h2 className='text-center text-[32px] pt-[10px] pb-[76px] max-lg:text-[20px]'>Como operar o D.A.R.P?</h2>
+
+          <h3 className='text-center text-[32px] bg-[#D9D9D9] rounded-lg px-10 py-10 mx-[80px] max-lg:text-[14px]'>Este dispositivo é projetado para ajudar pessoas com certos transtornos a controlar e reduzir comportamentos compulsivos, ansiedade exacerbada em momentos inoportunos, verificar portas várias vezes, lavar as mãos excessivamente e momentos de pânico. O D.A.R.P inclui as seguintes características:</h3>
+      </section>
+
+      <hr className='bg-[#D9D9D9] mt-[100px] mx-[120px] h-[8px] max-lg:mt-[25px]'/>
+
+      <section className='flex-col columns-2 justify-center items-center mt-[100px] mx-[80px] max-lg:columns-1 max-lg:mt-[25px]'>
+        <div className='justify-center'>
+          <img className='w-[453px] h-[690px] mx-auto max-lg:w-[220px] max-lg:h-[345px] max-lg:object-bottom' src={Prot1} alt="" />
+        </div>
+
+        <div className='mr-[80px] max-lg:mx-auto'>
+          <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[10px] py-[10px] text-center mb-10 mr-5 max-lg:text-[14px] max-lg:mt-5'>Vários botões foram implementados. Em sua grande parte podem ser personalizados de acordo com o uso do consumidor, mas em geral, eles podem contar com:</p>
+          <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[20px] pb-[15px] text-center mb-10 mx-[60px] max-lg:text-[20px]'>F.S: Feedback Sonoro;</p>
+          <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[20px] pb-[15px] text-center mb-10 mx-[60px] max-lg:text-[20px]'>B.I: Botão de interrupção;</p>
+          <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[20px] pb-[15px] text-center mb-10 mx-[60px] max-lg:text-[20px]'>F.V: Feedback Visual;</p>
+          <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[20px] pb-[15px] text-center mb-10 mx-[60px] max-lg:text-[20px]'>T.I: Temporizador Integrado;</p>
+          <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[20px] pb-[15px] text-center mb-10 mx-[60px] max-lg:text-[20px]'>D.R: Dados Registrador.</p>
+        </div>
+      </section>
+
+      <hr className='bg-[#D9D9D9] mt-[100px] mx-[120px] h-[8px] mb-20 max-lg:mt-[25px]'/>
+
+      <section className='flex-col columns-2 items-center justify-center max-lg:columns-1 max-lg:mx-[80px]'>
+        <p className='text-[32px] bg-[#D9D9D9] rounded-lg px-[25px] py-[20px] text-center mb-[320px] mt-[240px] ml-[100px] max-lg:text-[14px] max-lg:mt-[25px] max-lg:mb-[25px] max-lg:mx-auto'>Visando um de seus principais objetivos, sendo ele “simplicidade”, em suas laterais direta e esquerda, D.A.R.P aproveita de um simples layout. Sendo simples e induzível para ligar/desligar. Sem contar com a facilidade para aumentar ou reduzir o volume.</p>
+
+        <div className=''>
+          <img className='w-[421px] h-[722px] mx-auto max-lg:w-[225px] max-lg:h-[396px]' src={Prot2} alt="" />
+        </div>
+      </section>
+
+      <hr className='bg-[#D9D9D9] mt-[100px] mx-[120px] h-[8px] max-lg:mt-[25px]'/>
+
+      <section className='flex items-center justify-center mx-[80px] max-lg:mx-[80px] max-lg:mt-[25px]'>
+          
+        <div className=''>
+          <img className='w-[473px] h-[650px] max-lg:w-[220px] max-lg:h-[345px]' src={Prot3} alt="" />
+        </div>
+
+        <div className=''>
+          <img className='w-[473px] h-[650px] max-lg:w-[220px] max-lg:h-[345px]' src={Prot4} alt="" />
+        </div>
+
+      </section>
+
+
+      <div className='flex justify-center'>
+        <p className='text-[32px] text-center px-5 py-4 bg-[#D9D9D9] rounded-lg mt-[100px] mx-10 w-[1420px] max-lg:text-[14px] max-lg:mt-[25px]'>Pensando em diminuir a poluição causada pela produção do D.A.R.P, resolvemos fazer com que ele carregue via pilhas recarregáveis. Este é um ponto positivo, pois além de visar sua própria estética, a duração de carga também será aumentada drasticamente.</p>
+      </div>
+
+      
+
+      <hr className='bg-[#D9D9D9] mt-[100px] mx-[120px] h-[8px] max-lg:mt-[25px]'/>
+      
+      <section className='flex-col columns-2 justify-center mt-[80px] mx-auto max-lg:columns-1 max-lg:mx-[80px]'>
+
+        <div>
+          <img className='w-[473px] h-[650px] mx-auto max-lg:w-[260px] max-lg:h-[355px]' src={Prot5} alt="" />
+        </div>
+
+          <p className='text-center text-[32px] bg-[#D9D9D9] rounded-lg py-5 px-[40px] mr-[80px] mt-[200px] max-lg:text-[14px] max-lg:mt-[25px] max-lg:mx-auto mb-10'>Para indivíduos que possuem Transtorno Obsessivo Compulsivo, uma capa foi planejada para que a higiene do D.A.R.P possa ser mantida. Está capa é feita de silicone, um material satisfatório ao tocar e simples de lavar. </p>
+      </section>
+
+    </main>
   )
 }
