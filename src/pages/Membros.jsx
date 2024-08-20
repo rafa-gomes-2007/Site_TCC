97e2953a0044c094994073a8a994d9749250f72d
hick.hk <hick.hk@gmail.com>
2024-08-20 00:11:29 -0300

    pagina de membros feita

--- src/pages/Membros.jsx
+++ src/pages/Membros.jsx
@@ -6,50 +6,59 @@ import Kayque from '../assets/Kayque.svg'
 export default function Membros() {
   return (
 
-    <main className=" items-center justify-center flex flex-col w-screen">
+
+    <main className="items-center justify-center flex flex-col w-screen">
 
       <h1 className='mt-8 mb-4 text-6xl'>Membros</h1>
-      <p className='mb-8'>Quem são os integrantes que estão por trás do D.A.R.P</p>
+      <p className='mb-16 text-2xl'>Quem são os integrantes que estão por trás do D.A.R.P</p>
 
-      <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-        <p className='text-4xl'>Arthur Araújo</p>
+      <section className='flex justify-around gap-40 pb-32'>
 
-        <img src={Arthur} alt="Arthur" className='w-3/5  h-3/5'/>
-        
-        <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
+          
+          <p className='text-4xl '>Arthur Araújo</p>
 
-      </div>
+          <img src={Arthur} alt="Arthur" className='w-3/5  h-3/5'/>
+          
+          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
 
-      <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-        <p className='text-4xl'>Kayque Henrry</p>
+        </div>
 
-        <img src={Kayque} alt="Kayque" className='w-3/5  h-3/5'/>
-        
-        <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
+          <p className='text-4xl'>Kayque Henrry</p>
 
-      </div>
+          <img src={Kayque} alt="Kayque" className='w-3/5  h-3/5'/>
+          
+          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
 
-      <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-        <p className='text-4xl'>Rafael Gomes</p>
+        </div>
 
-        <img src={Rafael} alt="Rafawl" className='w-3/5  h-3/5'/>
-        
-        <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+      </section>
 
-      </div>
 
-      <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-        <p className='text-4xl'>Cauã Schunck</p>
+      <section className='flex justify-around gap-40 pb-12'>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
+          <p className='text-4xl'>Rafael Gomes</p>
 
-        <img src={Caua} alt="Caua" className='w-3/5  h-3/5'/>
-        
-        <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+          <img src={Rafael} alt="Rafawl" className='w-3/5  h-3/5'/>
+          
+          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
 
-      </div>
+        </div>
+
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
+          <p className='text-4xl'>Cauã Schunck</p>
+
+          <img src={Caua} alt="Caua" className='w-3/5  h-3/5'/>
+          
+          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+
+        </div>
+      </section>
+      
 
     </main>
 
     
-
-  )
-}
+   )
+  }
