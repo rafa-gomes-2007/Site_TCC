e96162d132ba819931d5a36d58fbc42bb0592f42
KayqueHenrry <102825082+KayqueHenrry@users.noreply.github.com>
2024-08-29 10:17:53 -0300

    Responsividade da página de membros finalizada

--- src/pages/Membros.jsx
+++ src/pages/Membros.jsx
@@ -9,49 +9,49 @@ export default function Membros() {
 
     <main className="items-center justify-center flex flex-col w-screen font-Questrial">
 
-      <h1 className='mt-8 text-[164px]'>MEMBROS</h1>
-      <p className='mb-20 text-[32px]'>Quem são os integrantes que estão por trás do D.A.R.P</p>
+      <h1 className='mt-8 text-[164px] max-lg:text-[70px]'>MEMBROS</h1>
+      <p className='text-center mb-20 text-[32px] max-lg:text-[20px] max-lg:mx-[40px]'>Quem são os integrantes que estão por trás do D.A.R.P</p>
 
-      <section className='flex justify-around gap-40 pb-32'>
+      <section className='flex justify-around gap-40 pb-32 max-lg:flex-col max-lg:columns-1'>
 
-        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[480px] rounded-[50px] space-y-12 max-lg:w-[318px] max-lg:h-[334] max-lg:space-y-1'>
           
-          <p className='text-4xl '>Arthur Araújo</p>
+          <h2 className='text-[52px] max-lg:text-[32px]'>Arthur Araújo</h2>
 
           <img src={Arthur} alt="Arthur" className='w-3/5  h-3/5'/>
           
-          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+          <a href="" className="bg-[#495057] text-white items-center flex justify-center rounded-[50px] text-[38px] px-8 py-4 max-lg:text-[24px] max-lg:px-4 max-lg:py-2">Veja Mais</a>
 
         </div>
 
-        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-          <p className='text-4xl'>Kayque Henrry</p>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[480px] rounded-[50px] space-y-12 max-lg:w-[318px] max-lg:h-[334] max-lg:space-y-1'>
+          <h2 className='text-[52px] max-lg:text-[32px]'>Kayque Henrry</h2>
 
           <img src={Kayque} alt="Kayque" className='w-3/5  h-3/5'/>
           
-          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+          <a href="" className="bg-[#495057] text-white items-center flex justify-center rounded-[50px] text-[38px] px-8 py-4 max-lg:text-[24px] max-lg:px-4 max-lg:py-2">Veja Mais</a>
 
         </div>
 
       </section>
 
 
-      <section className='flex justify-around gap-40 pb-12'>
-        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-          <p className='text-4xl'>Rafael Gomes</p>
+      <section className='flex justify-around gap-40 pb-12 max-lg:flex-col max-lg:columns-1'>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[480px] rounded-[50px] space-y-12 max-lg:w-[318px] max-lg:h-[334] max-lg:space-y-1'>
+          <p className='text-[52px] max-lg:text-[32px]'>Rafael Gomes</p>
 
           <img src={Rafael} alt="Rafawl" className='w-3/5  h-3/5'/>
           
-          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+          <a href="" className="bg-[#495057] text-white items-center flex justify-center rounded-[50px] text-[38px] px-8 py-4 max-lg:text-[24px] max-lg:px-4 max-lg:py-2">Veja Mais</a>
 
         </div>
 
-        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[400px] rounded-[50px] space-y-12'>
-          <p className='text-4xl'>Cauã Schunck</p>
+        <div className='bg-fundo_texto py-8 justify-center items-center flex-col flex w-[480px] rounded-[50px] space-y-12 max-lg:w-[318px] max-lg:h-[334] max-lg:space-y-1'>
+          <p className='text-[52px] max-lg:text-[32px]'>Cauã Schunck</p>
 
           <img src={Caua} alt="Caua" className='w-3/5  h-3/5'/>
           
-          <a href="" className="bg-black text-white w-48 h-16 items-center flex justify-center rounded-[50px] text-2xl">Veja Mais</a>
+          <a href="" className="bg-[#495057] text-white items-center flex justify-center rounded-[50px] text-[38px] px-8 py-4 max-lg:text-[24px] max-lg:px-4 max-lg:py-2">Veja Mais</a>
 
         </div>
       </section>
