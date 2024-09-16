f1b6790e70cae702c1186b5ad4be2bb5b5a6680b
KayqueHenrry <102825082+KayqueHenrry@users.noreply.github.com>
2024-09-16 10:17:24 -0300

    Update Bibliografia.jsx

--- src/pages/Bibliografia.jsx
+++ src/pages/Bibliografia.jsx
@@ -9,7 +9,7 @@ export default function Bibliografia() {
         <h2 className="text-[96px] Roboto-Flex max-lg:text-[62px]">T.O.C</h2>
         <p className="mb-8 text-center text-[30px] font-Questrial max-lg:text-[20px]">Transtorno Obsessivo Compulsivo</p>
         <p className="mb-12 text-center text-base text-[30px] font-Questrial max-lg:text-[18px]">O transtorno obsessivo-compulsivo (TOC) é representado por um grupo diverso de sintomas que incluem pensamentos intrusivos, rituais, preocupações e compulsões. Para serem consideradas como transtornos, essas obsessões ou compulsões devem causar sofrimento e disfuncionabilidade importante para o paciente, tanto na esfera ocupacional, quanto social ou relacional.</p>
-        <a href="" className="bg-black text-[22px] text-white w-[200px] h-[50px] items-center flex justify-center rounded-3xl ">
+        <a href="https://www.rededorsaoluiz.com.br/doencas/transtorno-obsessivo-compulsivo" className="bg-black text-[22px] text-white w-[200px] h-[50px] items-center flex justify-center rounded-3xl ">
           Veja Mais
         </a>
       </div>
@@ -18,7 +18,7 @@ export default function Bibliografia() {
         <h2 className="text-[96px] Roboto-Flex max-lg:text-[62px]">T.A.G</h2>
         <p className="mb-8 text-center text-[30px] font-Questrial max-lg:text-[20px]">Transtorno de Ansiedade Generalizada</p>
         <p className="mb-12 text-center text-base text-[30px] font-Questrial max-lg:text-[18px]">O transtorno de ansiedade de separação é caracterizado por ansiedade excessiva em relação ao afastamento dos pais ou seus substitutos, não adequada ao nível de desenvolvimento, que persiste por, no mínimo, quatro semanas, causando sofrimento intenso e prejuízos significativos em diferentes áreas da vida da criança ou adolescente.</p>
-        <a href="" className="bg-black text-[22px] text-white w-[200px] h-[50px] items-center flex justify-center rounded-3xl font-Questrial">
+        <a href="https://www.rededorsaoluiz.com.br/doencas/transtorno-de-ansiedade-generalizada" className="bg-black text-[22px] text-white w-[200px] h-[50px] items-center flex justify-center rounded-3xl font-Questrial">
           Veja Mais
         </a>
       </div>
@@ -27,7 +27,7 @@ export default function Bibliografia() {
         <h2 className="text-[96px] Roboto-Flex max-lg:text-[62px]">T.P</h2>
         <p className="mb-8 text-center text-[30px] font-Questrial max-lg:text-[20px]">Transtorno de Pânico</p>
         <p className="mb-12 text-center text-base text-[30px] font-Questrial max-lg:text-[18px]">O transtorno do pânico (TP) é caracterizado por crises de ansiedade repentina e intensa com forte sensação de medo ou mal-estar, acompanhadas de sintomas físicos. As crises podem ocorrer em qualquer lugar, contexto ou momento, durando em média de 15 a 30 minutos.</p>
-        <a href="" className="bg-black text-[22px] text-white w-[200px] h-[50px] items-center flex justify-center rounded-3xl font-Questrial">
+        <a href="https://www.rededorsaoluiz.com.br/doencas/sindrome-do-panico" className="bg-black text-[22px] text-white w-[200px] h-[50px] items-center flex justify-center rounded-3xl font-Questrial">
           Veja Mais
         </a>
       </div>
