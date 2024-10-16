25180419c1f5e850ac4b5f3a8617ab5e3e630cbb
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2024-10-16 11:50:03 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { List, X } from "@phosphor-icons/react";
-import { useState } from "react";
 import BotHome from "../assets/BotHome.png";
 import Membros from "../assets/Membros.png"
 import Book from "../assets/E-book.png";
@@ -9,6 +9,7 @@ import Mate from "../assets/Mate.png";
 import Logo from "../assets/Logo.png";
 
 export default function Header() {
+  
 
   const [abrirMenu, setAbrirMenu] = useState(false)
 
@@ -24,17 +25,17 @@ export default function Header() {
   }
 
   return (
-    <header className="bg-fundo_nav w-full font-Questrial flex items-center h-[150px] text-cinza_fonte rounded-b-2xl max-lg:h-auto max-lg:bg-cinza-back max-lg:justify-between">
-      <div className={`flex max-lg:bg-${abrirMenu ? "[##000000]" : "[#90969C] transition-all"} max-lg:w-full pb-[2px]`} >
+    <header className="bg-fundo_nav w-full font-Questrial flex items-center h-[150px] text-cinza_fonte rounded-b-2xl max-lg:h-auto max-lg:bg-cinza-back max-lg:justify-between fixed">
+      <div className={`flex max-lg:bg-[#90969C] max-lg:w-full pb-[10px]`} >
         <button className="mt-[-100%] max-lg:mt-3 max-lg:ml-2 bg-fundo_nav px-[10px] py-[10px] rounded-2xl max-lg:mr-2 h-[50px]" onClick={handleAbrir}>
-          {abrirMenu ? <List size={32} weight="bold"/> : <X size={32} weight="bold"/>}
+          {abrirMenu ? <X size={32} weight="bold"/> : <List size={32} weight="bold"/>}
         </button>
         <img className="justify-end mb-[2px] w-[100px] mr-0 max-lg:w-[55px] max-lg:h-[50px] max-lg:mt-3 ml-auto max-lg:mr-3" src={Logo} alt="" />
 
           
       </div>
 
-      <nav className={`flex items-center justify-center space-x-10 max-lg:flex-col max-lg:bg-[#90969C] transition-all max-lg:w-full max-lg:h-auto max-lg:fixed max-lg:space-x-0 max-lg:mt-[381px] max-lg:py-5 max-lg:gap-0 max-lg:ml-${abrirMenu ? "[700px]" : "[100px]"}`}>
+      <nav className={`flex items-center justify-center space-x-10 max-lg:flex-col max-lg:bg-[#90969C] transition-all max-lg:w-full max-lg:h-auto max-lg:fixed max-lg:space-x-0 max-lg:mt-[381px] max-lg:py-5 max-lg:gap-0`} style={{ right: abrirMenu ? '0' : '-150rem' }}>
 
         <hr className='bg-[#D9D9D9] h-[6px] max-lg:w-full mx-5 mb-4'/>
 
